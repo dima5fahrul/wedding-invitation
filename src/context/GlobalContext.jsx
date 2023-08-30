9f70ec69a9d124e168603773734c66feba2d9449
Dimas Fahrul <dfpa@gmail.com>
2023-08-30 21:14:47 +0700

    :bug: Fixing bug
    fixing bug

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -9,11 +9,11 @@ export const GlobalProvider = (props) => {
 
   // images
   const images = [
-    "../src/assets/first-img.jpg",
-    "../src/assets/second-img.jpg",
-    "../src/assets/third-img.jpg",
-    "../src/assets/fourth-img.jpg",
-    "../src/assets/fifth-img.jpg",
+    "src/assets/first-img.jpg",
+    "src/assets/second-img.jpg",
+    "src/assets/third-img.jpg",
+    "src/assets/fourth-img.jpg",
+    "src/assets/fifth-img.jpg",
   ];
 
   // additional
